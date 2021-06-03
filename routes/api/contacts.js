80ca297e3dddb6dae30119101830b1f8c178f8c1
mdcreator <63508140+mdcreator@users.noreply.github.com>
2021-06-03 22:31:52 +0300

    done hw2

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,24 +1,116 @@
-const express = require('express')
-const router = express.Router()
+import express from "express";
+const { Router } = express;
+import Contacts from "../../model/index.js";
+import validate from "./validation.js";
+const router = Router();
 
-router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.get("/", async (_req, res, next) => {
+  try {
+    const contacts = await Contacts.listContacts();
+    return res.status(200).json({
+      status: "success",
+      code: 200,
+      data: {
+        contacts,
+      },
+    });
+  } catch (e) {
+    next(e);
+  }
+});
 
-router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.get("/:contactId", async ({ params }, res, next) => {
+  try {
+    const contacts = await Contacts.getContactById(params.contactId);
+    if (contact) {
+      return res.status(200).json({
+        status: "success",
+        code: 200,
+        data: {
+          contacts,
+        },
+      });
+    } else {
+      return res.status(404).json({
+        status: "error",
+        code: 404,
+        message: "Not found",
+      });
+    }
+  } catch (e) {
+    next(e);
+  }
+});
 
-router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.post("/", validate.createContact, async ({ body }, res, next) => {
+  try {
+    // Присвокние ID новому контакту должно происходить в моделе при работе с базой данных, а не при обработке запроса!!!
+    const contact = await Contacts.addContact(body);
+    return res.status(201).json({
+      status: "success",
+      code: 201,
+      data: {
+        contact,
+      },
+    });
+  } catch (e) {
+    next(e);
+  }
+});
 
-router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.delete("/:contactId", async ({ params }, res, next) => {
+  try {
+    const contact = await Contacts.removeContact(params.contactId);
+    if (contact) {
+      return res.status(200).json({
+        status: "success",
+        code: 200,
+        message: "contact deleted",
+      });
+    } else {
+      return res.status(404).json({
+        status: "error",
+        code: 404,
+        message: "Not found",
+      });
+    }
+  } catch (e) {
+    next(e);
+  }
+});
 
-router.patch('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.patch(
+  "/:contactId",
+  validate.updateContact,
+  async ({ params, body }, res, next) => {
+    try {
+      if (!body.name && !body.email && !body.phone) {
+        return res.status(400).json({
+          status: "error",
+          code: 400,
+          message: "missing fields",
+        });
+      }
+      const contact = await Contacts.updateContact(params.contactId, body);
+      if (contact) {
+        return res.status(200).json({
+          status: "success",
+          code: 200,
+          data: {
+            contact,
+          },
+        });
+      } else {
+        return res.status(404).json({
+          status: "error",
+          code: 404,
+          message: "Not found",
+        });
+      }
+    } catch (e) {
+      next(e);
+    }
+  }
+);
 
-module.exports = router
+export default router;
